2408a36b888c73ea58575a40f520f0043eb75043
k2wl <k2wl@yahoo.com>
2013-06-25 23:56:34 +0530

    first commit

---
+++ include/generated/compile.h
@@ -0,0 +1,7 @@
+/* This file is auto generated, version 3 */
+/* SMP PREEMPT */
+#define UTS_MACHINE "arm"
+#define UTS_VERSION "#3 SMP PREEMPT Tue Jun 25 21:38:19 IST 2013"
+#define LINUX_COMPILE_BY "kar"
+#define LINUX_COMPILE_HOST "Android-ThinkPad-T400"
+#define LINUX_COMPILER "gcc version 4.7.4 20130603 (prerelease) (crosstool-NG hg+default-81ead21f86c2 - Linaro GCC 2013.06) "
