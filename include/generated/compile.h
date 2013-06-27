036aad0919fe1f5810f861d9017dae79e0245e60
k2wl <k2wl@yahoo.com>
2013-06-27 23:07:08 +0530

    f sync

--- include/generated/compile.h
+++ include/generated/compile.h
@@ -1,7 +1,7 @@
-/* This file is auto generated, version 4 */
+/* This file is auto generated, version 5 */
 /* SMP PREEMPT */
 #define UTS_MACHINE "arm"
-#define UTS_VERSION "#4 SMP PREEMPT Thu Jun 27 09:48:24 IST 2013"
+#define UTS_VERSION "#5 SMP PREEMPT Thu Jun 27 22:43:23 IST 2013"
 #define LINUX_COMPILE_BY "kar"
 #define LINUX_COMPILE_HOST "Android-ThinkPad-T400"
 #define LINUX_COMPILER "gcc version 4.7.4 20130603 (prerelease) (crosstool-NG hg+default-81ead21f86c2 - Linaro GCC 2013.06) "
